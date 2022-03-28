d39c272102b2e16f0d1dff9287d98b020e1e837e
twiglight <twig@light.com>
2022-03-29 01:16:11 +0200

    Initial commit

---
+++ config/emacs/init.el
@@ -0,0 +1,48 @@
+;;
+;; MELPA and use-package setup
+;;
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
+
+
+;;
+;; Theming
+;;
+(use-package apropospriate-theme
+  :ensure t
+  :config 
+  (load-theme 'apropospriate-dark t))
+
+
+;;
+;; Plugins
+;;
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
+
+
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
