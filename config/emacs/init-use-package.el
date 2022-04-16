6d680d041f9f8b5f5e441e5bdecddf248219fccb
twiglight <twig@light.xyz>
2022-04-16 16:03:59 +0200

    made some more changes

---
+++ config/emacs/init-use-package.el
@@ -0,0 +1,15 @@
+;;
+;; MELPA and use-package setup
+;;
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
--- config/emacs/init.el
+++ config/emacs/init.el
@@ -1,69 +1,40 @@
-;;
-;; MELPA and use-package setup
-;;
-(require 'package)
-(add-to-list 'package-archives
-	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(package-initialize)
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(eval-when-compile
-  (require 'use-package))
+;; Boilerplate for the use-package package
+(load (locate-user-emacs-file "init-use-package.el") nil :nomessage)
 
 ;;
 ;; Plugins
-;;
-(use-package which-key
-  :ensure t
-  :config
-  (which-key-mode))
-
-(use-package auto-complete
-  :ensure t
- )
-
-(use-package lsp-mode
-  :ensure t
-  :init
-  (setq lsp-keymap-prefix "C-c l")
-  :hook
-  (ruby-mode . lsp)
-  (lsp-mode . lsp-enable-which-key-integration)
-  :commands lsp)
-
-;;
-;; Hooks
-;;
-
-(add-hook 'prog-mode-hook 'display-line-numbers-mode) ;; Line numbers (duh)
-
-;;
-;; Theming
 ;;
 (use-package apropospriate-theme
   :ensure t
   :config 
   (load-theme 'apropospriate-dark t))
 
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
 
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode t))
 
+(use-package lsp-mode
+  :ensure t
+  :commands (lsp lsp-deferred)
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :config
+  (lsp-enable-which-key-integration))
 
+(use-package ruby-mode
+  :ensure t
+  :after lsp-mode
+  :hook (
+  (ruby-mode . lsp-deferred)))
 
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-
+;;
+;; Hooks
+;;
+(add-hook 'prog-mode-hook 'display-line-numbers-mode) ;; Line numbers (duh)
 
