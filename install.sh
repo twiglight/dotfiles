8047718f6ec0d3273f4924d5a3aaaa9208538dce
Wessel <wessel@localhost.localdomain>
2024-08-24 22:26:32 +0200

    Added zyp and some more finetuning

--- install.sh
+++ install.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 
-has_personal_bashrc_import=$(grep -E "^(source|\.) ~/.bashrc.personal$" ~/.bashrc)
+function has_personal_bashrc_import() {
+	grep -E "^(source|\.) ~/.bashrc.personal$" ~/.bashrc >> /dev/null 2>&1
+}
 
-if [ $? != 0 ]; then
+if ! has_personal_bashrc_import; then
+	echo "Updating .bashrc with personal file"
 	echo -e "\nsource ~/.bashrc.personal\n" >> ~/.bashrc
 fi
 
 ln -si "$(pwd)/bash/bashrc.personal" ~/.bashrc.personal
-
-# ln -si hyprland ~/.config/hypr
-
+ln -si "$(pwd)/hyprland" ~/.config/hypr
 ln -si "$(pwd)/vim/vimrc" ~/.vimrc
-
 ln -si "$(pwd)/waybar" ~/.config/waybar
-
-# ln -si sddm/themes/where_is_my_sddm_theme /usr/share/sddm/themes
-
-# ln -si "$(pwd)/bash/pac" ~/.local/bin/
-
+sudo ln -si "$(pwd)/sddm/themes/where_is_my_sddm_theme" /usr/share/sddm/themes
+ln -si "$(pwd)/pacman/pac" ~/.local/bin/
+ln -si "$(pwd)/zypper/zyp" ~/.local/bin/
 ln -si "$(pwd)/river" ~/.config/river
